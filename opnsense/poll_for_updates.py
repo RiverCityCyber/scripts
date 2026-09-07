0e1fc4b2d82133b96601adfda06d1148ceb6fdc7
River City Cyber <191951124+RiverCityCyber@users.noreply.github.com>
2026-09-06 20:40:09 -0400

    init commit

---
+++ opnsense/poll_for_updates.py
@@ -0,0 +1,11 @@
+#!/bin/env/venv
+
+#This is just a placeholder for now
+
+"""
+Project Goal: poll OPNSense every day, checking for updates.
+If updates are found, send message to local mattermost server
+If no updates found, exit with 200 or clean error message
+Have some other service poll this to check if it returns something other than 200
+Maybe systemd service?
+"""
